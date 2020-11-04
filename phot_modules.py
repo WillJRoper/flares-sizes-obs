03c10fb084ad7d4dffd6842dea55383af0d93bd2
wjr21 <wjr21@sussex.ac.uk>
2020-11-04 15:37:06 +0000

    uv-size relation plots

--- phot_modules.py
+++ phot_modules.py
@@ -554,14 +554,18 @@ def get_lum(sim, kappa, tag, BC_fac, IMF='Chabrier_300',
             bins=np.arange(-24, -16, 0.5), inp='FLARES', LF=True,
             filters=('FAKE.TH.FUV'), Type='Total', log10t_BC=7.,
             extinction='default', orientation="sim"):
-    try:
-        Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
-                   filters=filters, Type=Type, log10t_BC=log10t_BC,
-                   extinction=extinction, orientation=orientation)
+    # try:
+    #     Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
+    #                filters=filters, Type=Type, log10t_BC=log10t_BC,
+    #                extinction=extinction, orientation=orientation)
+    #
+    # except Exception as e:
+    #     Lums = np.ones(len(filters)) * np.nan
+    #     print(e)
 
-    except Exception as e:
-        Lums = np.ones(len(filters)) * np.nan
-        print(e)
+    Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
+               filters=filters, Type=Type, log10t_BC=log10t_BC,
+               extinction=extinction, orientation=orientation)
 
     if LF:
         tmp, edges = np.histogram(lum_to_M(Lums), bins=bins)
