5ddf0f0c944920546ced289174ef554262d2d880
wjr21 <wjr21@sussex.ac.uk>
2020-11-04 16:11:30 +0000

    uv-size relation plots

--- phot_modules.py
+++ phot_modules.py
@@ -44,11 +44,11 @@ def get_data(ii, tag, inp='FLARES'):
         if len(num) == 1:
             num = '0' + num
 
-        sim = rF"/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data/" \
+        sim = rF"/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data2/" \
               rF"FLARES_{num}_sp_info.hdf5"
 
     else:
-        sim = rF"/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data/" \
+        sim = rF"/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline/data2/" \
               rF"EAGLE_{inp}_sp_info.hdf5"
 
     with h5py.File(sim, 'r') as hf:
@@ -554,18 +554,14 @@ def get_lum(sim, kappa, tag, BC_fac, IMF='Chabrier_300',
             bins=np.arange(-24, -16, 0.5), inp='FLARES', LF=True,
             filters=('FAKE.TH.FUV'), Type='Total', log10t_BC=7.,
             extinction='default', orientation="sim"):
-    # try:
-    #     Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
-    #                filters=filters, Type=Type, log10t_BC=log10t_BC,
-    #                extinction=extinction, orientation=orientation)
-    #
-    # except Exception as e:
-    #     Lums = np.ones(len(filters)) * np.nan
-    #     print(e)
+    try:
+        Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
+                   filters=filters, Type=Type, log10t_BC=log10t_BC,
+                   extinction=extinction, orientation=orientation)
 
-    Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
-               filters=filters, Type=Type, log10t_BC=log10t_BC,
-               extinction=extinction, orientation=orientation)
+    except Exception as e:
+        Lums = np.ones(len(filters)) * np.nan
+        print(e)
 
     if LF:
         tmp, edges = np.histogram(lum_to_M(Lums), bins=bins)
