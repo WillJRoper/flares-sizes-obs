5ad8a9857d31bae43796593f4a34c3ca8ce85077
wjr21 <wjr21@sussex.ac.uk>
2020-11-10 16:13:20 +0000

    debug

--- UV_size_lumin_relation.py
+++ UV_size_lumin_relation.py
@@ -114,7 +114,7 @@ for tag in snaps:
         csoft = 0.001802390 / (0.6777 * (1 + z)) * 1e3
 
     # Define width
-    ini_width = 100 / (1 + z)
+    ini_width = 100
 
     # Compute the resolution
     ini_res = ini_width / csoft
