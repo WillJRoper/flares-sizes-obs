bdd8a14d62b61fcfe2730c187e3d75474647ceb6
wjr21 <wjr21@sussex.ac.uk>
2020-11-10 15:22:36 +0000

    debug

--- UV_size_lumin_relation.py
+++ UV_size_lumin_relation.py
@@ -148,9 +148,7 @@ for tag in snaps:
 
                 this_pos = poss[:, b: e].T * 10**3
                 this_lumin = reg_dict[f][b: e]
-                this_smls = reg_dict[f][b: e] * 10**3
-
-                print(this_lumin)
+                this_smls = smls[b: e] * 10**3
 
                 if np.nansum(this_lumin) == 0:
                     continue
