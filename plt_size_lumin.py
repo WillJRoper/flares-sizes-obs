d9490d4dd021b309320c65e230e3150736cd63df
wjr21 <wjr21@sussex.ac.uk>
2020-11-26 16:50:35 +0000

    added weightings

--- plt_size_lumin.py
+++ plt_size_lumin.py
@@ -447,7 +447,7 @@ for f in filters:
                     ax.plot(sden_lumins, r_from_surf_den(sden_lumins, sden), color="grey", linestyle="--", alpha=0.8)
                     print(m_to_flux(M_to_m(lum_to_M(sden), cosmo, z)) * u.nJy *u.kpc**-2)
                     print((m_to_flux(M_to_m(lum_to_M(sden), cosmo, z)) * u.nJy *u.kpc**-2) * cosmo.kpc_proper_per_arcmin(z)**2)
-                    print(((m_to_flux(M_to_m(lum_to_M(sden), cosmo, z)) * u.nJy *u.kpc**-2) * cosmo.kpc_proper_per_arcmin(z)**2).to(u.sr**-1))
+                    print(((m_to_flux(M_to_m(lum_to_M(sden), cosmo, z)) * u.nJy *u.kpc**-2) * cosmo.kpc_proper_per_arcmin(z)**2).to(u.nJy * u.sr**-1))
                     ax.text(10**29.85, r_from_surf_den(10**29.85, sden),
                             "%.3f" % np.log10(((M_to_m(lum_to_M(sden), cosmo, z) * u.kpc**-2) * cosmo.kpc_proper_per_arcmin(z)**2).to(u.sr**-1).value),
                             verticalalignment="center",
