62b0fcbca129ee9ae37cec560e645f9bde182f7b
wjr21 <wjr21@sussex.ac.uk>
2020-11-30 20:53:19 +0000

    added weightings

--- mock_detect.py
+++ mock_detect.py
@@ -178,9 +178,14 @@ for reg, snap in reg_snaps:
 
             img = imgs[i_img, :, :]
 
-            # threshold = phut.detect_threshold(img, nsigma=10, background=0)
+            threshold = phut.detect_threshold(img, nsigma=5, background=0)
 
-            segm = phut.detect_sources(img, np.percentile(img, 95), npixels=3)
+            segm = phut.detect_sources(img, threshold, npixels=5,
+                                       filter_kernel=kernel)
+            
+            segm = phut.deblend_sources(img, segm, npixels=5,
+                                        filter_kernel=kernel,
+                                        nlevels=32, contrast=0.001)
 
             fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
             ax1.grid(False)
