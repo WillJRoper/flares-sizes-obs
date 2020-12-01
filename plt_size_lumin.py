cf612be93a91b050418ec58e2fc9b91f61304429
wjr21 <wjr21@sussex.ac.uk>
2020-12-01 12:24:12 +0000

    added weightings

--- plt_size_lumin.py
+++ plt_size_lumin.py
@@ -219,7 +219,7 @@ for reg, snap in reg_snaps:
         masses = orientation_group[f]["Mass"][...]
         okinds = masses > masslim
 
-        print(masses.size, masses[okinds].size)
+        print(reg, snap, f, masses[okinds].size)
 
         hlr_dict[snap][f].extend(orientation_group[f]["HLR_0.5"][...][okinds])
         hlr_app_dict[snap][f].extend(
