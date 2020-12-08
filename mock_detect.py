9ef219b8897118eb567e1834ce3803e5c83db358
wjr21 <wjr21@sussex.ac.uk>
2020-12-08 00:21:49 +0000

    including visible filter

--- mock_detect.py
+++ mock_detect.py
@@ -287,7 +287,7 @@ for reg, snap in reg_snaps:
 
             img = imgs[i_img, :, :]
 
-            img[img < 10**19] = 0
+            img[img < 10**21] = 0
 
             # threshold = phut.detect_threshold(img, nsigma=5)
             threshold = np.median(img) + np.std(img)
@@ -297,7 +297,7 @@ for reg, snap in reg_snaps:
                                            filter_kernel=kernel)
                 segm = phut.deblend_sources(img, segm, npixels=5,
                                             filter_kernel=kernel,
-                                            nlevels=32, contrast=0.001)
+                                            nlevels=32, contrast=0.000001)
             except TypeError:
                 continue
             # x_cent = []
