861403691f6b39dfbc9dd02db0410c13794dcd4a
wjr21 <wjr21@sussex.ac.uk>
2021-07-14 11:21:20 +0100

    updated kernel image function

--- phot_modules.py
+++ phot_modules.py
@@ -151,12 +151,8 @@ def lum(sim, kappa, tag, BC_fac, inp='FLARES', IMF='Chabrier_300', LF=True,
     cops = cops / (1 + z)
 
     # --- create rest-frame luminosities
-    print("WHY CANT WE GET HERE", filters)
     F = flare.filters.add_filters(filters, new_lam=model.lam)
-    # print("WHY CANT WE GET HERE")
-    # print(F.filters)
-    # model.create_Lnu_grid(F)  # --- create new L grid for each filter. In units of erg/s/Hz
-    print("WHY CANT WE GET HERE")
+    model.create_Lnu_grid(F)  # --- create new L grid for each filter. In units of erg/s/Hz
     for jj in range(len(begin)):
         print(jj)
 
