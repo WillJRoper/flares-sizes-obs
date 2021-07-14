19ab06c11a42aa68d5119601cd9b650d7c614db0
wjr21 <wjr21@sussex.ac.uk>
2021-07-14 14:49:48 +0100

    updated kernel image function

--- get_hlr_data_int.sh
+++ get_hlr_data_int.sh
@@ -21,12 +21,7 @@ source activate flares-env
 i=$(($SLURM_ARRAY_TASK_ID - 1))
 
 # Run the program
-#./UV_size_lumin_relation_distributed.py $i sim Total 0
-#./UV_size_lumin_relation_distributed.py $i sim Total 1
-#./UV_size_lumin_relation_distributed.py $i sim Total 2
-#./UV_size_lumin_relation_distributed.py $i sim Intrinsic 0
-#./UV_size_lumin_relation_distributed.py $i sim Intrinsic 1
-#./UV_size_lumin_relation_distributed.py $i sim Intrinsic 2
+./UV_size_lumin_relation_distributed.py $i sim Intrinsic
 #./UV_size_lumin_relation_distributed.py $i face-on Total
 #./UV_size_lumin_relation_distributed.py $i face-on Intrinsic
 
