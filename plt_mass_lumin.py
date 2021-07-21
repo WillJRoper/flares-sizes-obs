ba26d5b7342acdea0e41f5eb19c8a9ee7c2274d0
wjr21 <wjr21@sussex.ac.uk>
2021-07-21 16:21:00 +0100

    updated data paths in plot scripts

--- plt_mass_lumin.py
+++ plt_mass_lumin.py
@@ -175,38 +175,38 @@ for f in filters:
             bbox_inches='tight')
 
         plt.close(fig)
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        try:
-            cbar = ax.hexbin(mass, nstar,
-                             gridsize=50, mincnt=1, C=w,
-                             reduce_C_function=np.sum,
-                             xscale='log', yscale='log',
-                             norm=LogNorm(), linewidths=0.2,
-                             cmap='viridis', alpha=0.9)
-            ax.axhline(nlim, linestyle="--", color="k", alpha=0.7)
-        except ValueError as e:
-            print(e)
-            continue
-
-        ax.text(0.95, 0.05, f'$z={z}$',
-                bbox=dict(boxstyle="round,pad=0.3", fc='w',
-                          ec="k", lw=1, alpha=0.8),
-                transform=ax.transAxes, horizontalalignment='right',
-                fontsize=8)
-
-        # Label axes
-        ax.set_ylabel(r'$N_\star$')
-        ax.set_xlabel('$M_\star/ M_\odot$')
-
-        ax.tick_params(axis='both', which='minor', bottom=True, left=True)
-
-        fig.savefig(
-            'plots/' + str(z) + '/MassNStar_' + f + '_' + str(
-                z) + '_'
-            + orientation + '_' + Type + "_" + extinction + "_"
-            + '%d.png' % nlim,
-            bbox_inches='tight')
-
-        plt.close(fig)
+        #
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        # try:
+        #     cbar = ax.hexbin(mass, nstar,
+        #                      gridsize=50, mincnt=1, C=w,
+        #                      reduce_C_function=np.sum,
+        #                      xscale='log', yscale='log',
+        #                      norm=LogNorm(), linewidths=0.2,
+        #                      cmap='viridis', alpha=0.9)
+        #     ax.axhline(nlim, linestyle="--", color="k", alpha=0.7)
+        # except ValueError as e:
+        #     print(e)
+        #     continue
+        #
+        # ax.text(0.95, 0.05, f'$z={z}$',
+        #         bbox=dict(boxstyle="round,pad=0.3", fc='w',
+        #                   ec="k", lw=1, alpha=0.8),
+        #         transform=ax.transAxes, horizontalalignment='right',
+        #         fontsize=8)
+        #
+        # # Label axes
+        # ax.set_ylabel(r'$N_\star$')
+        # ax.set_xlabel('$M_\star/ M_\odot$')
+        #
+        # ax.tick_params(axis='both', which='minor', bottom=True, left=True)
+        #
+        # fig.savefig(
+        #     'plots/' + str(z) + '/MassNStar_' + f + '_' + str(
+        #         z) + '_'
+        #     + orientation + '_' + Type + "_" + extinction + "_"
+        #     + '%d.png' % nlim,
+        #     bbox_inches='tight')
+        #
+        # plt.close(fig)
