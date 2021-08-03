5b36c34c1e6e082d4417e1ea0fdc6772f539a66c
wjr21 <wjr21@sussex.ac.uk>
2021-08-03 15:44:10 +0100

    script for Steve's SFR problem

---
+++ Steve.py
@@ -0,0 +1,27 @@
+#!/cosma/home/dp004/dc-rope1/.conda/envs/flares-env/bin/python
+import os
+import warnings
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+os.environ['FLARE'] = '/cosma7/data/dp004/dc-wilk2/flare'
+
+matplotlib.use('Agg')
+warnings.filterwarnings('ignore')
+import seaborn as sns
+import h5py
+
+
+sns.set_context("paper")
+sns.set_style('whitegrid')
+
+
+hdf = h5py.File("/cosma7/data/dp004/dc-payy1/my_files/"
+                "flares_pipeline/data/flares.hdf5", "r")
+
+for key in hdf.keys():
+    print(key, hdf[key].keys())
+
+
