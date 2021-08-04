e1330db1b5c628f90b913464a2813fd30d0a3bad
wjr21 <wjr21@sussex.ac.uk>
2021-08-04 18:21:54 +0100

    image grid

--- plt_img_grid.py
+++ plt_img_grid.py
@@ -147,7 +147,7 @@ for reg in regions:
                     # pltimg = np.zeros_like(this_imgs[ind, :, :])
                     # pltimg[this_imgs[ind, :, :] > 0] = np.log10(this_imgs[ind, :, :][this_imgs[ind, :, :] > 0])
                     if ind > -1:
-                        axes[i, j].imshow(this_imgs[ind, :, :], cmap=cmr.cosmic, norm=norm)
+                        axes[i, j].imshow(this_imgs[ind, 10:-10, 10:-10], cmap=cmr.cosmic, norm=norm)
 
                         string = r"$\log_{10}\left(M_\star/M_\odot\right) =$ %.2f" % np.log10(
                             this_mass[ind]) + "\n" \
