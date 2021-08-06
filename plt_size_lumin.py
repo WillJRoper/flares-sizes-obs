183fbe7299f2ca3f0c72163c9aa1b4b651a99417
wjr21 <wjr21@sussex.ac.uk>
2021-08-06 13:26:58 +0100

    image grid

--- plt_size_lumin.py
+++ plt_size_lumin.py
@@ -158,7 +158,7 @@ filters = ('FAKE.TH.FUV', 'FAKE.TH.NUV', 'FAKE.TH.V')
 
 csoft = 0.001802390 / (0.6777) * 1e3
 
-nlim = 700
+nlim = 10**9
 
 hlr_dict = {}
 hlr_app_dict = {}
@@ -215,7 +215,7 @@ for reg, snap in reg_snaps:
         weight_dict[snap].setdefault(f, [])
 
         masses = hdf[f]["Mass"][...]
-        okinds = hdf[f]["nStar"][...] > nlim
+        okinds = masses > nlim
 
         print(reg, snap, f, masses[okinds].size)
 
