a6fa09f50a2b6bc759021d05bf63304a72fc7ca9
wjr21 <wjr21@sussex.ac.uk>
2021-08-10 21:41:08 +0100

    image grid

--- plt_half_dust_radius_comp.py
+++ plt_half_dust_radius_comp.py
@@ -137,7 +137,7 @@ for snap in snaps:
         ax = fig.add_subplot(111)
         # ax.loglog()
         try:
-            cbar = ax.pcolormesh((xbin_cents, ybin_cents), Z=H,
+            cbar = ax.pcolormesh((xbin_cents, ybin_cents), C=H,
                                norm=LogNorm(), cmap='Greys')
             # ax.hexbin(hdrs[okinds1], hlrs[okinds1], gridsize=50, mincnt=1, C=w,
             #           reduce_C_function=np.sum, xscale='log', yscale='log',
