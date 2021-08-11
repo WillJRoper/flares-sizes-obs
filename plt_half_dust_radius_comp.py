edf10d3ace5398241404ae9b5201374f493b583f
wjr21 <wjr21@sussex.ac.uk>
2021-08-11 01:46:36 +0100

    image grid

--- plt_half_dust_radius_comp.py
+++ plt_half_dust_radius_comp.py
@@ -121,7 +121,7 @@ for snap in snaps:
         okinds1 = masses >= 10**9
         okinds2 = masses < 10 ** 9
 
-        bins = np.logspace(np.log10(0.08), np.log10(30), 30)
+        bins = np.logspace(np.log10(0.08), np.log10(30), 15)
         H, xbins, ybins = np.histogram2d(hdrs[okinds2], hlrs[okinds2],
                                          bins=bins, weights=w[okinds2])
 
@@ -189,9 +189,9 @@ for snap in snaps:
 
         ratio = hlrs / hdrs
 
-        bins = np.logspace(np.log10(0.08), np.log10(30), 30)
+        bins = np.logspace(np.log10(0.08), np.log10(30), 15)
         ratio_bins = np.logspace(np.log10(np.min(ratio[okinds2])),
-                                 np.log10(np.max(ratio[okinds2])), 30)
+                                 np.log10(np.max(ratio[okinds2])), 15)
 
         H, xbins, ybins = np.histogram2d(hlrs[okinds2], ratio[okinds2],
                                          bins=(bins, ratio_bins),
