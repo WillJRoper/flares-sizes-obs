092764c164a601cfd8d92ae209bef4ed5aa27ad2
wjr21 <wjr21@sussex.ac.uk>
2021-10-07 20:28:50 +0100

    cubic spline

--- phot_modules.py
+++ phot_modules.py
@@ -588,11 +588,11 @@ def get_lines(sim, kappa, tag, BC_fac, inp='FLARES', IMF='Chabrier_300',
 def get_lum(sim, kappa, tag, BC_fac, IMF='Chabrier_300',
             bins=np.arange(-24, -16, 0.5), inp='FLARES', LF=True,
             filters=('FAKE.TH.FUV',), Type='Total', log10t_BC=7.,
-            extinction='default', orientation="sim", masslim=None):
+            extinction='default', orientation="sim", masslim=None, nlim=None):
     Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
                filters=filters, Type=Type, log10t_BC=log10t_BC,
                extinction=extinction, orientation=orientation,
-               masslim=masslim)
+               masslim=masslim, nlim=nlim)
 
     # try:
     #     Lums = lum(sim, kappa, tag, BC_fac=BC_fac, IMF=IMF, inp=inp, LF=LF,
