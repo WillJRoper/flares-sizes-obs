515714a9bf6678eee68b4f83407aabd957959dc1
wjr21 <wjr21@sussex.ac.uk>
2021-11-22 13:36:13 +0000

    filter comparison sizes plot

--- plt_size_lumin_grid_all_filters.py
+++ plt_size_lumin_grid_all_filters.py
@@ -369,8 +369,8 @@ def size_lumin_grid_allf(data, intr_data, snaps, filters, orientation,
             uni_legend_elements.append(l)
             included.append((l.get_label(), l.get_marker()))
 
-    axes[2].legend(handles=uni_legend_elements, loc='upper center',
-                   bbox_to_anchor=(0.5, -0.3), fancybox=True,
+    axes_ratio[2].legend(handles=uni_legend_elements, loc='upper center',
+                   bbox_to_anchor=(0.5, -0.15), fancybox=True,
                    ncol=len(uni_legend_elements))
 
     fig.savefig(
