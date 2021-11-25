5acebe0ffd9be9654bfcd941e39175d7b7d9cd79
wjr21 <wjr21@sussex.ac.uk>
2021-11-25 10:45:43 +0000

    fixed hexbin extent

--- plt_stack_img_grid.py
+++ plt_stack_img_grid.py
@@ -70,147 +70,147 @@ snaps = ['007_z008p000', '010_z005p000']
 
 np.random.seed(100)
 
-for snap in snaps:
-
-    reg = regions[0]
-
-    hdf = h5py.File(
-        "data/flares_sizes_{}_{}_{}_{}.hdf5".format(reg, snap, Type,
-                                                    orientation),
-        "r")
-
-    f = filters[0]
-    img_shape = hdf[f]["Images"].shape[-1]
-
-    hdf.close()
-
-    dpi = img_shape
-    fig = plt.figure(figsize=(4, len(filters)), dpi=dpi)
-    fig_log = plt.figure(figsize=(4, len(filters)), dpi=dpi)
-    gs = gridspec.GridSpec(ncols=4, nrows=len(filters))
-    gs.update(wspace=0.0, hspace=0.0)
-    axes = np.empty((len(filters), 4), dtype=object)
-    axes_log = np.empty((len(filters), 4), dtype=object)
-    bins = [10 ** 8, 10 ** 9, 10 ** 9.5, 10 ** 10, np.inf]
-
-    stacks = {}
-    num_stacked = {}
-
-    for f in filters:
-
-        stacks[f] = {}
-        num_stacked[f] = {}
-        for b in bins[:-1]:
-            stacks[f][b] = np.zeros((img_shape, img_shape))
-            num_stacked[f][b] = 0
-
-        for reg in regions:
-
-            print(snap, f, reg)
-
-            reg = regions[0]
-
-            hdf = h5py.File(
-                "data/flares_sizes_{}_{}_{}_{}.hdf5".format(reg, snap,
-                                                            Type,
-                                                            orientation),
-                "r")
-
-            for i, b in enumerate(bins[:-1]):
-                masses = hdf[f]["Mass"][...]
-                okinds = np.logical_and(masses >= b,
-                                        masses < bins[i + 1])
-                imgs = hdf[f]["Images"][...]
-                stacks[f][b] += np.sum(imgs[okinds, :, :],
-                                       axis=0)
-
-                num_stacked[f][b] += masses[okinds].size
-
-            hdf.close()
-
-    z_str = snap.split('z')[1].split('p')
-    z = float(z_str[0] + '.' + z_str[1])
-
-    all_imgs = []
-    for f in filters:
-        for b in bins[:-1]:
-            stacks[f][b] = stacks[f][b] / num_stacked[f][b]
-            all_imgs.append(stacks[f][b])
-    all_imgs = np.array(all_imgs)
-    print(all_imgs.shape)
-    norm = cm.Normalize(vmin=0,
-                        vmax=np.percentile(all_imgs[all_imgs > 0], 99.99),
-                        clip=True)
-    norm_log = cm.LogNorm(vmin=0,
-                          vmax=np.percentile(all_imgs[all_imgs > 0], 99),
-                          clip=True)
-
-    for i in range(len(filters)):
-        for j in range(4):
-            axes[i, j] = fig.add_subplot(gs[i, j])
-            axes_log[i, j] = fig_log.add_subplot(gs[i, j])
-
-            # Remove axis labels and ticks
-            axes[i, j].tick_params(axis='x', top=False, bottom=False,
-                                   labeltop=False, labelbottom=False)
-            axes[i, j].tick_params(axis='y', left=False, right=False,
-                                   labelleft=False, labelright=False)
-
-            # Remove axis labels and ticks
-            axes_log[i, j].tick_params(axis='x', top=False,
-                                       bottom=False,
-                                       labeltop=False,
-                                       labelbottom=False)
-            axes_log[i, j].tick_params(axis='y', left=False,
-                                       right=False,
-                                       labelleft=False,
-                                       labelright=False)
-
-    for j, b in enumerate(bins[:-1]):
-        for i, f in enumerate(filters):
-
-            if j == 0:
-                axes[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
-                axes_log[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
-            if i == 0:
-                if bins[j + 1] == np.inf:
-                    axes[i, j].set_title("%d $\leq "
-                                         "\log_{10}(M/M_\odot)$"
-                                         % (np.log10(bins[j])), fontsize=6)
-                    axes_log[i, j].set_title("%d $\leq "
-                                             "\log_{10}(M/M_\odot)$"
-                                             % (np.log10(bins[j])), fontsize=6)
-                else:
-                    axes[i, j].set_title("%d $\leq "
-                                         "\log_{10}(M/M_\odot) <$ %d"
-                                         % (np.log10(bins[j]),
-                                            np.log10(bins[j + 1])), fontsize=6)
-                    axes_log[i, j].set_title("%d $\leq "
-                                         "\log_{10}(M/M_\odot) <$ %d"
-                                         % (np.log10(bins[j]),
-                                            np.log10(bins[j + 1])), fontsize=6)
-
-            size = stacks[f][b].shape[0]
-            axes[i, j].imshow(stacks[f][b][int(0.4 * size):-int(0.4 * size),
-                              int(0.4 * size):-int(0.4 * size)],
-                              cmap=cmr.neutral_r)
-
-            axes_log[i, j].imshow(
-                stacks[f][b][int(0.3 * size):-int(0.3 * size),
-                int(0.3 * size):-int(0.3 * size)],
-                cmap=cmr.neutral, norm=cm.LogNorm())
-
-    fig.savefig(
-        'plots/Image_grids/StackImgGrid_' + reg
-        + '_' + snap + '_' + orientation + '_' + Type
-        + "_" + extinction + "".replace(".", "p") + ".png",
-        bbox_inches='tight', dpi=fig.dpi)
-    fig_log.savefig(
-        'plots/Image_grids/StackLogImgGrid_' + reg
-        + '_' + snap + '_' + orientation + '_' + Type
-        + "_" + extinction + "".replace(".", "p") + ".png",
-        bbox_inches='tight', dpi=fig_log.dpi)
-    plt.close(fig)
+# for snap in snaps:
+#
+#     reg = regions[0]
+#
+#     hdf = h5py.File(
+#         "data/flares_sizes_{}_{}_{}_{}.hdf5".format(reg, snap, Type,
+#                                                     orientation),
+#         "r")
+#
+#     f = filters[0]
+#     img_shape = hdf[f]["Images"].shape[-1]
+#
+#     hdf.close()
+#
+#     dpi = img_shape
+#     fig = plt.figure(figsize=(4, len(filters)), dpi=dpi)
+#     fig_log = plt.figure(figsize=(4, len(filters)), dpi=dpi)
+#     gs = gridspec.GridSpec(ncols=4, nrows=len(filters))
+#     gs.update(wspace=0.0, hspace=0.0)
+#     axes = np.empty((len(filters), 4), dtype=object)
+#     axes_log = np.empty((len(filters), 4), dtype=object)
+#     bins = [10 ** 8, 10 ** 9, 10 ** 9.5, 10 ** 10, np.inf]
+#
+#     stacks = {}
+#     num_stacked = {}
+#
+#     for f in filters:
+#
+#         stacks[f] = {}
+#         num_stacked[f] = {}
+#         for b in bins[:-1]:
+#             stacks[f][b] = np.zeros((img_shape, img_shape))
+#             num_stacked[f][b] = 0
+#
+#         for reg in regions:
+#
+#             print(snap, f, reg)
+#
+#             reg = regions[0]
+#
+#             hdf = h5py.File(
+#                 "data/flares_sizes_{}_{}_{}_{}.hdf5".format(reg, snap,
+#                                                             Type,
+#                                                             orientation),
+#                 "r")
+#
+#             for i, b in enumerate(bins[:-1]):
+#                 masses = hdf[f]["Mass"][...]
+#                 okinds = np.logical_and(masses >= b,
+#                                         masses < bins[i + 1])
+#                 imgs = hdf[f]["Images"][...]
+#                 stacks[f][b] += np.sum(imgs[okinds, :, :],
+#                                        axis=0)
+#
+#                 num_stacked[f][b] += masses[okinds].size
+#
+#             hdf.close()
+#
+#     z_str = snap.split('z')[1].split('p')
+#     z = float(z_str[0] + '.' + z_str[1])
+#
+#     all_imgs = []
+#     for f in filters:
+#         for b in bins[:-1]:
+#             stacks[f][b] = stacks[f][b] / num_stacked[f][b]
+#             all_imgs.append(stacks[f][b])
+#     all_imgs = np.array(all_imgs)
+#     print(all_imgs.shape)
+#     norm = cm.Normalize(vmin=0,
+#                         vmax=np.percentile(all_imgs[all_imgs > 0], 99.99),
+#                         clip=True)
+#     norm_log = cm.LogNorm(vmin=0,
+#                           vmax=np.percentile(all_imgs[all_imgs > 0], 99),
+#                           clip=True)
+#
+#     for i in range(len(filters)):
+#         for j in range(4):
+#             axes[i, j] = fig.add_subplot(gs[i, j])
+#             axes_log[i, j] = fig_log.add_subplot(gs[i, j])
+#
+#             # Remove axis labels and ticks
+#             axes[i, j].tick_params(axis='x', top=False, bottom=False,
+#                                    labeltop=False, labelbottom=False)
+#             axes[i, j].tick_params(axis='y', left=False, right=False,
+#                                    labelleft=False, labelright=False)
+#
+#             # Remove axis labels and ticks
+#             axes_log[i, j].tick_params(axis='x', top=False,
+#                                        bottom=False,
+#                                        labeltop=False,
+#                                        labelbottom=False)
+#             axes_log[i, j].tick_params(axis='y', left=False,
+#                                        right=False,
+#                                        labelleft=False,
+#                                        labelright=False)
+#
+#     for j, b in enumerate(bins[:-1]):
+#         for i, f in enumerate(filters):
+#
+#             if j == 0:
+#                 axes[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
+#                 axes_log[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
+#             if i == 0:
+#                 if bins[j + 1] == np.inf:
+#                     axes[i, j].set_title("%d $\leq "
+#                                          "\log_{10}(M/M_\odot)$"
+#                                          % (np.log10(bins[j])), fontsize=6)
+#                     axes_log[i, j].set_title("%d $\leq "
+#                                              "\log_{10}(M/M_\odot)$"
+#                                              % (np.log10(bins[j])), fontsize=6)
+#                 else:
+#                     axes[i, j].set_title("%d $\leq "
+#                                          "\log_{10}(M/M_\odot) <$ %d"
+#                                          % (np.log10(bins[j]),
+#                                             np.log10(bins[j + 1])), fontsize=6)
+#                     axes_log[i, j].set_title("%d $\leq "
+#                                          "\log_{10}(M/M_\odot) <$ %d"
+#                                          % (np.log10(bins[j]),
+#                                             np.log10(bins[j + 1])), fontsize=6)
+#
+#             size = stacks[f][b].shape[0]
+#             axes[i, j].imshow(stacks[f][b][int(0.4 * size):-int(0.4 * size),
+#                               int(0.4 * size):-int(0.4 * size)],
+#                               cmap=cmr.neutral_r)
+#
+#             axes_log[i, j].imshow(
+#                 stacks[f][b][int(0.3 * size):-int(0.3 * size),
+#                 int(0.3 * size):-int(0.3 * size)],
+#                 cmap=cmr.neutral, norm=cm.LogNorm())
+#
+#     fig.savefig(
+#         'plots/Image_grids/StackImgGrid_' + reg
+#         + '_' + snap + '_' + orientation + '_' + Type
+#         + "_" + extinction + "".replace(".", "p") + ".png",
+#         bbox_inches='tight', dpi=fig.dpi)
+#     fig_log.savefig(
+#         'plots/Image_grids/StackLogImgGrid_' + reg
+#         + '_' + snap + '_' + orientation + '_' + Type
+#         + "_" + extinction + "".replace(".", "p") + ".png",
+#         bbox_inches='tight', dpi=fig_log.dpi)
+#     plt.close(fig)
 
 for f in filters:
 
@@ -334,26 +334,26 @@ for f in filters:
         for j, b in enumerate(bins[:-1]):
             for i, f in enumerate(row_filters):
 
-                if j == 0:
-                    axes[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
-                    axes_log[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
+                # if j == 0:
+                #     axes[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
+                #     axes_log[i, j].set_ylabel(f.split(".")[-1], fontsize=6)
                 if i == 0:
                     if bins[j + 1] == np.inf:
                         axes[i, j].set_title("%d $\leq "
                                              "\log_{10}(M/M_\odot)$"
-                                             % (np.log10(bins[j])), fontsize=6)
+                                             % (np.log10(bins[j])), fontsize=5)
                         axes_log[i, j].set_title("%d $\leq "
                                                  "\log_{10}(M/M_\odot)$"
-                                                 % (np.log10(bins[j])), fontsize=6)
+                                                 % (np.log10(bins[j])), fontsize=5)
                     else:
                         axes[i, j].set_title("%d $\leq "
                                              "\log_{10}(M/M_\odot) <$ %d"
                                              % (np.log10(bins[j]),
-                                                np.log10(bins[j + 1])), fontsize=6)
+                                                np.log10(bins[j + 1])), fontsize=5)
                         axes_log[i, j].set_title("%d $\leq "
                                              "\log_{10}(M/M_\odot) <$ %d"
                                              % (np.log10(bins[j]),
-                                                np.log10(bins[j + 1])), fontsize=6)
+                                                np.log10(bins[j + 1])), fontsize=5)
 
                 size = stacks[f][b].shape[0]
                 axes[i, j].imshow(stacks[f][b][int(0.4 * size):-int(0.4 * size),
