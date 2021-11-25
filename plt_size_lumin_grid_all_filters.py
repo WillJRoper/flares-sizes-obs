b1b5ff6387d9bffb1a7b5c0360341c47e96b21ff
wjr21 <wjr21@sussex.ac.uk>
2021-11-25 15:43:31 +0000

    fixed hexbin extent

--- plt_size_lumin_grid_all_filters.py
+++ plt_size_lumin_grid_all_filters.py
@@ -289,7 +289,7 @@ def size_lumin_grid_allf(data, intr_data, snaps, filters, orientation,
                 fit_lumins = np.logspace(np.log10(np.min(lumins)),
                                          np.log10(np.max(lumins)),
                                          1000)
-                
+                print(np.log10(np.min(lumins)), np.log10(np.max(lumins)))
                 print(np.log10(fit_lumins))
 
                 fit = st_line_fit(fit_lumins, popt[0], popt[1])
@@ -340,7 +340,7 @@ def size_lumin_grid_allf(data, intr_data, snaps, filters, orientation,
             if f == filters[-1]:
                 axes[i].hexbin(lumins,
                                hlrs, gridsize=50,
-                               mincnt=1,
+                               mincnt=0.00001,
                                C=w,
                                reduce_C_function=np.sum,
                                xscale='log', yscale='log',
