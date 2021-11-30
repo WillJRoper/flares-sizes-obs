4b5a24ba5d332c6956e3a30b9ca0030b7708ed71
wjr21 <wjr21@sussex.ac.uk>
2021-11-30 17:25:01 +0000

    fixed hexbin extent

--- plt_size_evo_violin.py
+++ plt_size_evo_violin.py
@@ -471,7 +471,7 @@ def size_evo_violin(data, intr_data, snaps, f, mtype, orientation, Type, extinct
     ax.tick_params(axis='y', which='minor', left=True)
 
     ax.set_xlim(4.77, 11.5)
-    ax.set_ylim(10 ** -0.8, 10 ** 1.)
+    # ax.set_ylim(10 ** -0.8, 10 ** 1.)
 
     ax.legend(handles=legend_elements, loc='upper center',
               bbox_to_anchor=(0.5, -0.15), fancybox=True, ncol=3)
