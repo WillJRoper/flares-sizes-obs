cc2f180e5ab5a379e5288ef56fc053d9efe52ca7
wjr21 <wjr21@sussex.ac.uk>
2021-12-07 15:56:26 +0000

    fixed hexbin extent

--- plt_stack_img_grid.py
+++ plt_stack_img_grid.py
@@ -242,8 +242,12 @@ for f in filters:
             reg = regions[0]
 
             hdf = h5py.File(
-                "data/flares_sizes_{}_{}_{}_{}.hdf5".format(reg, snap, Type,
-                                                            orientation),
+                "data/flares_sizes_all_{}_{}_{}_{}_{}.hdf5".format(reg, snap,
+                                                                   Type,
+                                                                   orientation,
+                                                                   f.split(
+                                                                       ".")[
+                                                                       -1]),
                 "r")
 
             f = row_filters[0]
