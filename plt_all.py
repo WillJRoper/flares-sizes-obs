8bd74837462dfaade10f77be6e03796c9f7979f6
wjr21 <wjr21@sussex.ac.uk>
2022-01-06 15:31:41 +0000

    testing 95% completeness

--- plt_all.py
+++ plt_all.py
@@ -60,222 +60,222 @@ for reg in range(0, 40):
     else:
         regions.append(str(reg))
 
-reg_snaps = []
-for reg in reversed(regions):
-
-    for snap in all_snaps:
-        reg_snaps.append((reg, snap))
-
-# Define dictionary for image size
-img_shapes = {}
-
-for reg, snap in reg_snaps:
-
-    for f in all_filters:
-
-        try:
-            hdf = h5py.File(
-                "data/flares_sizes_all_{}_{}_{}_{}_{}.hdf5".format(reg, snap,
-                                                                   "Total",
-                                                                   orientation,
-                                                                   f.split(
-                                                                       ".")[
-                                                                       -1]),
-                "r")
-        except OSError as e:
-            print(reg, snap, e)
-            continue
-
-        data.setdefault(snap, {})
-        intr_data.setdefault(snap, {})
-
-        data[snap].setdefault(f, {})
-        intr_data[snap].setdefault(f, {})
-
-        for key in keys:
-            try:
-                data[snap][f].setdefault(key, []).extend(hdf[f][key][...])
-            except KeyError as e:
-                print(reg, snap, e)
-
-        img_shape = hdf[f]["Images"].shape
-        if len(img_shape) > 2:
-            img_shapes[snap] = (img_shape[1],
-                                img_shape[2])
-
-        data[snap][f].setdefault("Weight", []).extend(
-            np.full(hdf[f]["Mass"][...].size, weights[int(reg)]))
-
-        hdf.close()
-
-        try:
-            hdf = h5py.File(
-                "data/flares_sizes_all_{}_{}_{}_{}_{}.hdf5".format(reg, snap,
-                                                                   "Intrinsic",
-                                                                   orientation,
-                                                                   f.split(
-                                                                       ".")[
-                                                                       -1]),
-                "r")
-        except OSError as e:
-            print(reg, snap, e)
-            continue
-
-        surf_dens = hdf[f]["Luminosity"][...] \
-                    / (np.pi * (2 * hdf[f]["HLR_0.5"][...]) ** 2)
-
-        intr_data[snap][f].setdefault("Inner_Surface_Density",
-                                      []).extend(surf_dens)
-
-        intr_data[snap][f].setdefault("Weight", []).extend(
-            np.full(hdf[f]["Mass"][...].size, weights[int(reg)]))
-
-        for key in keys:
-            try:
-                intr_data[snap][f].setdefault(key, []).extend(hdf[f][key][...])
-            except KeyError as e:
-                print(reg, snap, e)
-
-        hdf.close()
-
-# Count the number of galaxies in FLARES
-all_n_gals = 0
-all_n_gals_above_100 = 0
-all_comp_gals = 0
-all_diffuse_gals = 0
-for snap in intr_data.keys():
-    okinds = np.array(intr_data[snap][filters[0]]["Mass"]) > 10 ** 8
-    n_gals = np.array(intr_data[snap][filters[0]]["Mass"])[okinds].size
-    all_n_gals += n_gals
-    okinds = np.array(intr_data[snap][filters[0]]["nStar"]) > 100
-    n_gals_above_100 = np.array(intr_data[snap][filters[0]]["Mass"])[
-        okinds].size
-    all_n_gals_above_100 += n_gals_above_100
-
-    okinds = np.array(intr_data[snap][filters[0]]["nStar"]) > 100
-    sample = np.array(intr_data[snap][filters[0]][
-                          "Inner_Surface_Density"])[okinds]
-    sfd_okinds = sample >= 10 ** 29
-    comp_gals = sample[sfd_okinds].size
-    diffuse_gals = sample[~sfd_okinds].size
-
-    all_comp_gals += comp_gals
-    all_diffuse_gals += diffuse_gals
-
-    print("Galaxies with M_star/M_sun>10**8 in snapshot %s: %d"
-          % (snap, n_gals))
-    print("Galaxies with N_star>100 in snapshot %s: %d"
-          % (snap, n_gals_above_100))
-    print("Compact galaxies in snapshot %s: %d"
-          % (snap, comp_gals))
-    print("Diffuse galaxies in snapshot %s: %d"
-          % (snap, diffuse_gals))
-
-print("Total galaxies with M_star/M_sun>10**8: %d" % all_n_gals)
-print("Total galaxies with N_star>100: %d" % all_n_gals_above_100)
-print("Total compact galaxies: %d" % all_comp_gals)
-print("Total diffuse galaxies: %d" % all_diffuse_gals)
-
-print("Image Dimensions:")
-print(img_shapes)
-
-for snap in all_snaps:
-
-    for f in all_filters:
-        okinds = np.ones(len(intr_data[snap][f]["nStar"]), dtype=bool)
-        for key in keys:
-            okinds = np.logical_and(okinds, np.logical_and(
-                np.array(data[snap][f][key]) > 0,
-                np.array(intr_data[snap][f][key]) > 0))
-
-        for key in data[snap][f].keys():
-            data[snap][f][key] = np.array(data[snap][f][key])[okinds]
-
-        for key in intr_data[snap][f].keys():
-            intr_data[snap][f][key] = np.array(intr_data[snap][f][key])[okinds]
-
-        okinds = intr_data[snap][f]["nStar"] >= 100
-
-        data[snap][f]["okinds"] = okinds
-        intr_data[snap][f]["okinds"] = okinds
-
-        data[snap][f]["Complete_Luminosity"] = np.percentile(
-            data[snap][f]["Image_Luminosity"][~okinds], 95)
-        data[snap][f]["Complete_Mass"] = np.percentile(
-            data[snap][f]["Mass"][~okinds], 95)
-        intr_data[snap][f]["Complete_Luminosity"] = np.percentile(
-            intr_data[snap][f]["Image_Luminosity"][~okinds], 95)
-        intr_data[snap][f]["Complete_Mass"] = np.percentile(
-            intr_data[snap][f]["Mass"][~okinds], 95)
-
-        # data[snap][f]["Complete_Luminosity"] = np.max(
-        #     data[snap][f]["Image_Luminosity"][~okinds])
-        # data[snap][f]["Complete_Mass"] = np.max(
-        #     data[snap][f]["Mass"][~okinds])
-        # intr_data[snap][f]["Complete_Luminosity"] = np.max(
-        #     intr_data[snap][f]["Image_Luminosity"][~okinds])
-        # intr_data[snap][f]["Complete_Mass"] = np.max(
-        #     intr_data[snap][f]["Mass"][~okinds])
-
-        print("Intrinsic: complete luminosity/mass for", snap, f,
-              "%.2f/%.2f" % (
-                  np.log10(intr_data[snap][f]["Complete_Luminosity"]),
-                  np.log10(intr_data[snap][f]["Complete_Mass"])))
-        print("Total: complete luminosity/mass for", snap, f,
-              "%.2f/%.2f" % (np.log10(data[snap][f]["Complete_Luminosity"]),
-                             np.log10(data[snap][f]["Complete_Mass"])))
-        print("----------------------------------------------------------")
-
-        okinds = np.logical_and(
-            data[snap][f]["Image_Luminosity"] >= data[snap][f][
-                "Complete_Luminosity"], data[snap][f]["Mass"] >= data[snap][f][
-                "Complete_Mass"])
-        intr_okinds = np.logical_and(
-            intr_data[snap][f]["Image_Luminosity"] >= intr_data[snap][f][
-                "Complete_Luminosity"],
-            intr_data[snap][f]["Mass"] >= intr_data[snap][f]["Complete_Mass"])
-
-        compact_pop = np.array(
-            intr_data[snap][f]["Inner_Surface_Density"]) >= 10 ** 29
-        diffuse_pop = np.array(
-            intr_data[snap][f]["Inner_Surface_Density"]) < 10 ** 29
-
-        data[snap][f]["Compact_Population"] = compact_pop
-        data[snap][f]["Diffuse_Population"] = diffuse_pop
-        intr_data[snap][f]["Compact_Population"] = compact_pop
-        intr_data[snap][f]["Diffuse_Population"] = diffuse_pop
-
-        data[snap][f]["Compact_Population_Complete"] = np.logical_and(
-            compact_pop, okinds)
-        data[snap][f]["Diffuse_Population_Complete"] = np.logical_and(
-            diffuse_pop, okinds)
-        intr_data[snap][f]["Compact_Population_Complete"] = np.logical_and(
-            compact_pop, intr_okinds)
-        intr_data[snap][f]["Diffuse_Population_Complete"] = np.logical_and(
-            diffuse_pop, intr_okinds)
-
-        data[snap][f]["Compact_Population_NotComplete"] = np.logical_and(
-            compact_pop, ~okinds)
-        data[snap][f]["Diffuse_Population_NotComplete"] = np.logical_and(
-            diffuse_pop, ~okinds)
-        intr_data[snap][f]["Compact_Population_NotComplete"] = np.logical_and(
-            compact_pop, ~intr_okinds)
-        intr_data[snap][f]["Diffuse_Population_NotComplete"] = np.logical_and(
-            diffuse_pop, ~intr_okinds)
-
-# Count the number of galaxies in FLARES
-all_complete_gals = 0
-for snap in intr_data.keys():
-    okinds = np.logical_or(
-        data[snap][filters[0]]["Compact_Population_Complete"],
-        data[snap][filters[0]]["Diffuse_Population_Complete"])
-    complete_gals = data[snap][filters[0]]["Mass"][okinds].size
-    all_complete_gals += complete_gals
-
-    print("Complete Galaxies in snapshot %s: %d"
-          % (snap, complete_gals))
-
-print("Total Complete galaxies: %d" % all_complete_gals)
+# reg_snaps = []
+# for reg in reversed(regions):
+#
+#     for snap in all_snaps:
+#         reg_snaps.append((reg, snap))
+#
+# # Define dictionary for image size
+# img_shapes = {}
+#
+# for reg, snap in reg_snaps:
+#
+#     for f in all_filters:
+#
+#         try:
+#             hdf = h5py.File(
+#                 "data/flares_sizes_all_{}_{}_{}_{}_{}.hdf5".format(reg, snap,
+#                                                                    "Total",
+#                                                                    orientation,
+#                                                                    f.split(
+#                                                                        ".")[
+#                                                                        -1]),
+#                 "r")
+#         except OSError as e:
+#             print(reg, snap, e)
+#             continue
+#
+#         data.setdefault(snap, {})
+#         intr_data.setdefault(snap, {})
+#
+#         data[snap].setdefault(f, {})
+#         intr_data[snap].setdefault(f, {})
+#
+#         for key in keys:
+#             try:
+#                 data[snap][f].setdefault(key, []).extend(hdf[f][key][...])
+#             except KeyError as e:
+#                 print(reg, snap, e)
+#
+#         img_shape = hdf[f]["Images"].shape
+#         if len(img_shape) > 2:
+#             img_shapes[snap] = (img_shape[1],
+#                                 img_shape[2])
+#
+#         data[snap][f].setdefault("Weight", []).extend(
+#             np.full(hdf[f]["Mass"][...].size, weights[int(reg)]))
+#
+#         hdf.close()
+#
+#         try:
+#             hdf = h5py.File(
+#                 "data/flares_sizes_all_{}_{}_{}_{}_{}.hdf5".format(reg, snap,
+#                                                                    "Intrinsic",
+#                                                                    orientation,
+#                                                                    f.split(
+#                                                                        ".")[
+#                                                                        -1]),
+#                 "r")
+#         except OSError as e:
+#             print(reg, snap, e)
+#             continue
+#
+#         surf_dens = hdf[f]["Luminosity"][...] \
+#                     / (np.pi * (2 * hdf[f]["HLR_0.5"][...]) ** 2)
+#
+#         intr_data[snap][f].setdefault("Inner_Surface_Density",
+#                                       []).extend(surf_dens)
+#
+#         intr_data[snap][f].setdefault("Weight", []).extend(
+#             np.full(hdf[f]["Mass"][...].size, weights[int(reg)]))
+#
+#         for key in keys:
+#             try:
+#                 intr_data[snap][f].setdefault(key, []).extend(hdf[f][key][...])
+#             except KeyError as e:
+#                 print(reg, snap, e)
+#
+#         hdf.close()
+#
+# # Count the number of galaxies in FLARES
+# all_n_gals = 0
+# all_n_gals_above_100 = 0
+# all_comp_gals = 0
+# all_diffuse_gals = 0
+# for snap in intr_data.keys():
+#     okinds = np.array(intr_data[snap][filters[0]]["Mass"]) > 10 ** 8
+#     n_gals = np.array(intr_data[snap][filters[0]]["Mass"])[okinds].size
+#     all_n_gals += n_gals
+#     okinds = np.array(intr_data[snap][filters[0]]["nStar"]) > 100
+#     n_gals_above_100 = np.array(intr_data[snap][filters[0]]["Mass"])[
+#         okinds].size
+#     all_n_gals_above_100 += n_gals_above_100
+#
+#     okinds = np.array(intr_data[snap][filters[0]]["nStar"]) > 100
+#     sample = np.array(intr_data[snap][filters[0]][
+#                           "Inner_Surface_Density"])[okinds]
+#     sfd_okinds = sample >= 10 ** 29
+#     comp_gals = sample[sfd_okinds].size
+#     diffuse_gals = sample[~sfd_okinds].size
+#
+#     all_comp_gals += comp_gals
+#     all_diffuse_gals += diffuse_gals
+#
+#     print("Galaxies with M_star/M_sun>10**8 in snapshot %s: %d"
+#           % (snap, n_gals))
+#     print("Galaxies with N_star>100 in snapshot %s: %d"
+#           % (snap, n_gals_above_100))
+#     print("Compact galaxies in snapshot %s: %d"
+#           % (snap, comp_gals))
+#     print("Diffuse galaxies in snapshot %s: %d"
+#           % (snap, diffuse_gals))
+#
+# print("Total galaxies with M_star/M_sun>10**8: %d" % all_n_gals)
+# print("Total galaxies with N_star>100: %d" % all_n_gals_above_100)
+# print("Total compact galaxies: %d" % all_comp_gals)
+# print("Total diffuse galaxies: %d" % all_diffuse_gals)
+#
+# print("Image Dimensions:")
+# print(img_shapes)
+#
+# for snap in all_snaps:
+#
+#     for f in all_filters:
+#         okinds = np.ones(len(intr_data[snap][f]["nStar"]), dtype=bool)
+#         for key in keys:
+#             okinds = np.logical_and(okinds, np.logical_and(
+#                 np.array(data[snap][f][key]) > 0,
+#                 np.array(intr_data[snap][f][key]) > 0))
+#
+#         for key in data[snap][f].keys():
+#             data[snap][f][key] = np.array(data[snap][f][key])[okinds]
+#
+#         for key in intr_data[snap][f].keys():
+#             intr_data[snap][f][key] = np.array(intr_data[snap][f][key])[okinds]
+#
+#         okinds = intr_data[snap][f]["nStar"] >= 100
+#
+#         data[snap][f]["okinds"] = okinds
+#         intr_data[snap][f]["okinds"] = okinds
+#
+#         data[snap][f]["Complete_Luminosity"] = np.percentile(
+#             data[snap][f]["Image_Luminosity"][~okinds], 95)
+#         data[snap][f]["Complete_Mass"] = np.percentile(
+#             data[snap][f]["Mass"][~okinds], 95)
+#         intr_data[snap][f]["Complete_Luminosity"] = np.percentile(
+#             intr_data[snap][f]["Image_Luminosity"][~okinds], 95)
+#         intr_data[snap][f]["Complete_Mass"] = np.percentile(
+#             intr_data[snap][f]["Mass"][~okinds], 95)
+#
+#         # data[snap][f]["Complete_Luminosity"] = np.max(
+#         #     data[snap][f]["Image_Luminosity"][~okinds])
+#         # data[snap][f]["Complete_Mass"] = np.max(
+#         #     data[snap][f]["Mass"][~okinds])
+#         # intr_data[snap][f]["Complete_Luminosity"] = np.max(
+#         #     intr_data[snap][f]["Image_Luminosity"][~okinds])
+#         # intr_data[snap][f]["Complete_Mass"] = np.max(
+#         #     intr_data[snap][f]["Mass"][~okinds])
+#
+#         print("Intrinsic: complete luminosity/mass for", snap, f,
+#               "%.2f/%.2f" % (
+#                   np.log10(intr_data[snap][f]["Complete_Luminosity"]),
+#                   np.log10(intr_data[snap][f]["Complete_Mass"])))
+#         print("Total: complete luminosity/mass for", snap, f,
+#               "%.2f/%.2f" % (np.log10(data[snap][f]["Complete_Luminosity"]),
+#                              np.log10(data[snap][f]["Complete_Mass"])))
+#         print("----------------------------------------------------------")
+#
+#         okinds = np.logical_and(
+#             data[snap][f]["Image_Luminosity"] >= data[snap][f][
+#                 "Complete_Luminosity"], data[snap][f]["Mass"] >= data[snap][f][
+#                 "Complete_Mass"])
+#         intr_okinds = np.logical_and(
+#             intr_data[snap][f]["Image_Luminosity"] >= intr_data[snap][f][
+#                 "Complete_Luminosity"],
+#             intr_data[snap][f]["Mass"] >= intr_data[snap][f]["Complete_Mass"])
+#
+#         compact_pop = np.array(
+#             intr_data[snap][f]["Inner_Surface_Density"]) >= 10 ** 29
+#         diffuse_pop = np.array(
+#             intr_data[snap][f]["Inner_Surface_Density"]) < 10 ** 29
+#
+#         data[snap][f]["Compact_Population"] = compact_pop
+#         data[snap][f]["Diffuse_Population"] = diffuse_pop
+#         intr_data[snap][f]["Compact_Population"] = compact_pop
+#         intr_data[snap][f]["Diffuse_Population"] = diffuse_pop
+#
+#         data[snap][f]["Compact_Population_Complete"] = np.logical_and(
+#             compact_pop, okinds)
+#         data[snap][f]["Diffuse_Population_Complete"] = np.logical_and(
+#             diffuse_pop, okinds)
+#         intr_data[snap][f]["Compact_Population_Complete"] = np.logical_and(
+#             compact_pop, intr_okinds)
+#         intr_data[snap][f]["Diffuse_Population_Complete"] = np.logical_and(
+#             diffuse_pop, intr_okinds)
+#
+#         data[snap][f]["Compact_Population_NotComplete"] = np.logical_and(
+#             compact_pop, ~okinds)
+#         data[snap][f]["Diffuse_Population_NotComplete"] = np.logical_and(
+#             diffuse_pop, ~okinds)
+#         intr_data[snap][f]["Compact_Population_NotComplete"] = np.logical_and(
+#             compact_pop, ~intr_okinds)
+#         intr_data[snap][f]["Diffuse_Population_NotComplete"] = np.logical_and(
+#             diffuse_pop, ~intr_okinds)
+#
+# # Count the number of galaxies in FLARES
+# all_complete_gals = 0
+# for snap in intr_data.keys():
+#     okinds = np.logical_or(
+#         data[snap][filters[0]]["Compact_Population_Complete"],
+#         data[snap][filters[0]]["Diffuse_Population_Complete"])
+#     complete_gals = data[snap][filters[0]]["Mass"][okinds].size
+#     all_complete_gals += complete_gals
+#
+#     print("Complete Galaxies in snapshot %s: %d"
+#           % (snap, complete_gals))
+#
+# print("Total Complete galaxies: %d" % all_complete_gals)
 
 # Define the norm
 weight_norm = LogNorm(vmin=10 ** -4, vmax=1)
