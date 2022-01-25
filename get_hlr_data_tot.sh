15734eff4cb5ae33b65a2dc873a0f5823acb6651
wjr21 <wjr21@sussex.ac.uk>
2022-01-25 13:37:49 +0000

    fixed hexbin extent

--- get_hlr_data_tot.sh
+++ get_hlr_data_tot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --ntasks 12 # The number of cores you need...
-#SBATCH --array=1-480
-#SBATCH -p cosma6 #or some other partition, e.g. cosma, cosma6, etc.
+#SBATCH --array=1-480%40
+#SBATCH -p cosma7 #or some other partition, e.g. cosma, cosma6, etc.
 #SBATCH -A dp004
 #SBATCH --cpus-per-task=1
 #SBATCH -J FLARES-Sizes #Give it something meaningful.
