a226c5bc70c9fb07bb130bceb9961d7660b0eff8
wjr21 <wjr21@sussex.ac.uk>
2022-01-27 17:26:59 +0000

    plot updates

--- plt_sed.py
+++ plt_sed.py
@@ -18,7 +18,18 @@ import h5py
 import matplotlib.gridspec as gridspec
 from flare import plt as flareplt
 plt.rcParams['axes.grid'] = True
-plt.rcParams['font.size'] = 12
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 filter_path = "/cosma7/data/dp004/dc-wilk2/flare/data/filters/"
 
