8901f6dc8593ac79a27bd96c8a9a937b63dffb24
wjr21 <wjr21@sussex.ac.uk>
2022-01-27 18:14:39 +0000

    plot updates

--- plt_img_grid.py
+++ plt_img_grid.py
@@ -254,14 +254,14 @@ for reg in regions:
                                               cmap=cmr.neutral,
                                               norm=norm_log)
 
-            axes[3, 3].plot([0.05, 0.15], [0.025, 0.025], lw=0.1, color='k',
+            axes[3, 3].plot([0.05, 0.15], [0.025, 0.025], lw=1, color='k',
                     clip_on=False,
                     transform=axes[3, 3].transAxes)
 
-            axes[3, 3].plot([0.05, 0.05], [0.022, 0.027], lw=0.15, color='k',
+            axes[3, 3].plot([0.05, 0.05], [0.022, 0.027], lw=1, color='k',
                     clip_on=False,
                     transform=axes[3, 3].transAxes)
-            axes[3, 3].plot([0.15, 0.15], [0.022, 0.027], lw=0.15, color='k',
+            axes[3, 3].plot([0.15, 0.15], [0.022, 0.027], lw=1, color='k',
                     clip_on=False,
                     transform=axes[3, 3].transAxes)
 
