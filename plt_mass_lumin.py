aff2e6edf1699034a297df9742bc3721cc68ce7b
wjr21 <wjr21@sussex.ac.uk>
2022-02-03 14:41:00 +0000

    added 1D profiles to img stack

--- plt_mass_lumin.py
+++ plt_mass_lumin.py
@@ -15,7 +15,22 @@ import pandas as pd
 from matplotlib.colors import LogNorm
 import matplotlib.gridspec as gridspec
 from flare import plt as flareplt
+
+
+# Set plotting fontsizes
 plt.rcParams['axes.grid'] = True
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 
 def mass_lumin(mass, lumins, com_comp, diff_comp, com_ncomp, diff_ncomp, okinds, w,
