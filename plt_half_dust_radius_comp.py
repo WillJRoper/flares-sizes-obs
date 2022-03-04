037de1661069940962ba39bc1458d7dd225167ab
wjr21 <wjr21@sussex.ac.uk>
2022-03-04 15:18:03 +0000

    plot updates

--- plt_half_dust_radius_comp.py
+++ plt_half_dust_radius_comp.py
@@ -239,7 +239,7 @@ def hdr_comp(hdrs, hlrs, hlrints, w, com_comp, diff_comp, com_ncomp,
     ratio = hlrs / hlrints
 
     fig = plt.figure(figsize=(3.5, 2 * 3.5))
-    gs = gridspec.GridSpec(2, 2, width_ratios=[60, 1])
+    gs = gridspec.GridSpec(2, 2, width_ratios=[30, 1])
     gs.update(wspace=0.0, hspace=0.0)
     ax1 = fig.add_subplot(gs[0, 0])
     ax2 = fig.add_subplot(gs[1, 0])
--- plt_size_type_comp.py
+++ plt_size_type_comp.py
@@ -37,7 +37,7 @@ def size_comp(f, snap, hlrs, hlrs_pix, w, com_comp, diff_comp, com_ncomp,
     z = float(z_str[0] + '.' + z_str[1])
 
     fig = plt.figure(figsize=(3.5, 2 * 3.5))
-    gs = gridspec.GridSpec(2, 2, width_ratios=[60, 1])
+    gs = gridspec.GridSpec(2, 2, width_ratios=[30, 1])
     gs.update(wspace=0.0, hspace=0.0)
     ax1 = fig.add_subplot(gs[0, 0])
     ax2 = fig.add_subplot(gs[1, 0])
