fa2a5dd1a027bd1a56a2dcbbee7f58b7f0736c42
wjr21 <wjr21@sussex.ac.uk>
2022-03-22 12:59:36 +0000

    plot updates

--- plt_size_lumin_grid.py
+++ plt_size_lumin_grid.py
@@ -364,7 +364,7 @@ def size_lumin_grid(data, snaps, filters, orientation, Type, extinction,
                 included.append((l.get_label(), l.get_marker()))
 
         axes_comb[2].legend(handles=uni_legend_elements, loc='upper center',
-                       bbox_to_anchor=(0.5, -0.15), fancybox=True,
+                       bbox_to_anchor=(0.5, -0.4), fancybox=True,
                        ncol=3)
 
         cb1 = mpl.colorbar.ColorbarBase(cax1, cmap=plt.get_cmap("Greys"),
