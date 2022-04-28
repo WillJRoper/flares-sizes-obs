0014e11b4eecab4cd3d19c12d46341a77fb9e56e
wjr21 <wjr21@sussex.ac.uk>
2022-04-28 15:15:48 +0100

    added scale length fitting to stack plots

--- plt_stack_profile_fit.py
+++ plt_stack_profile_fit.py
@@ -274,7 +274,8 @@ for f in filters:
 
         # Define mass bins
         bin_wid = 10**0.2
-        bins = np.arange(10**8, 10**11.8, bin_wid)
+        bins = np.arange(8, 11.8, np.log10(bin_wid))
+        bins = [10**b for b in bins]
         bin_cents = (bins[1:] + bins[:-1]) / 2
 
         stacks = {}
